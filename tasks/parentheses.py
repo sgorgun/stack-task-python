20dd19053a6fdcc3a431e97177afce499c2f790e
sgorgun <sergei.gorgun@gmail.com>
2024-03-25 00:33:43 +0200

    Refactor parentheses validation and RPN evaluation

--- tasks/parentheses.py
+++ tasks/parentheses.py
@@ -1,5 +1,5 @@
 """Template for programming assignment: parentheses validation."""
-
+from tasks.stack import Stack, LinkedListNode
 
 def is_valid_parentheses(expression: str) -> bool:
     """Returns True if a given string is valid parentheses expression.
@@ -9,4 +9,18 @@ def is_valid_parentheses(expression: str) -> bool:
     Returns:
         bool, whether a given string is valid parentheses expression.
     """
-    pass
+    stack = Stack()
+    parentheses_simbols = {')': '(', ']': '[', '}': '{'}
+    open_parentheses = set(parentheses_simbols.values())
+
+    for char in expression:
+        if char in open_parentheses:
+            stack.push(char)
+        elif char in parentheses_simbols:
+            if stack.empty() or stack.peak() != parentheses_simbols[char]:
+                return False
+            stack.pop()
+        else:
+            continue
+
+    return stack.empty()
